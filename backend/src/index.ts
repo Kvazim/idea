790faa43ee9d36efed342987e265711560c3bdc1
Vladimir <mesvladimir@yandex.ru>
2024-11-23 18:02:46 +0300

    установка express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello');
+import express from 'express';
+
+const expressApp = express();
+
+expressApp.listen(3000, () => {
+  console.info('Listineng at https://localhost:3000')
+});
+
+expressApp.get('/ping', (req, res) => {
+  res.send('pong');
+});
